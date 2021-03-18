1179871239fc75117de77bbf33c51faaea127db8
Kunki Oh <kunkiya@gmail.com>
2021-03-18 09:00:05 +0900

    이상한 문자 만들기

---
+++ level1_2021_3_18.js
@@ -0,0 +1,14 @@
+//이상한 문자 만들기
+function solution(s) {
+    let answer = '';
+    const arr = s.split(' ');
+    for(let i=0; i<arr.length; i++){
+        const string = arr[i].split('');
+        for(let i=0; i<string.length; i++){
+        let str = string[i];
+        answer += i%2 === 0 ? str.toUpperCase() : str.toLowerCase();
+        }
+        answer += arr.length - 1 === i ? "" : " ";
+    }
+    return answer;
+}
