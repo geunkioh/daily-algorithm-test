5afca566903415c8b1beb132a35eb530b011ce1d
kunkioh <kunkiya@gmail.com>
2021-03-21 19:17:39 +0900

    자연수 뒤집어 배열로 만들기

--- level1_2021_3_20.js
+++ level1_2021_3_20.js
@@ -1,4 +1,5 @@
 //자연수 뒤집어 배열로 만들기
+//내장함수 o
 function solution(n) {
     const arr =`${n}`.split('');
     let answer = [];
@@ -7,4 +8,14 @@ function solution(n) {
     }
     const reverse = answer.reverse();
     return reverse;
+}
+
+// 내장함수 x
+function solution(n) {
+    const arr =`${n}`;
+    let answer = [];
+    for(let i=arr.length-1; i>=0; i--){
+        answer.push(arr[i]*1)
+    }
+    return answer;
 }
