74dbecaa65104945bfa8ba71691f6b9de087b546
Kunki Oh <kunkiya@gmail.com>
2021-03-26 09:19:39 +0900

    문자열 내 p와 y의 개수

---
+++ level1_2021_3_8.js
@@ -0,0 +1,12 @@
+//문자열 내 p와 y의 개수
+function solution(s) {
+    let p = 0, y = 0;
+    for (let i of s) {
+        const ea = i.toLowerCase();
+        ea === "p" && p++;
+        ea === "y" && y++;
+    }
+    const answer = p === y ? true : false;
+    return answer;
+}
+
